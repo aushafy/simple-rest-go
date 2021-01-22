f9bda5e79eed5c93dcf3cb66d93b823f73c69d96
aushafy <aushafy@gmail.com>
2021-01-22 14:18:15 +0700

    version 1

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-  "net/http"
-  "github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  r := gin.Default()
+	r := gin.Default()
 
-  r.GET("/", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{"data": "hello world"})    
-  })
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "hello world guys!"})
+	})
 
-  r.Run()
+	r.Run()
 }
