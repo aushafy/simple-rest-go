cf12c3e47599a1443f2d02bdcc36ea2826188936
aushafy <aushafy@gmail.com>
2021-01-27 08:38:39 +0700

    v2

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"data": "hello world guys...!"})
+		c.JSON(http.StatusOK, gin.H{"data": "hello world guys...! Version 2"})
 	})
 
 	r.Run()
